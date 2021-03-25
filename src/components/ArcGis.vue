99f9e6dfb219d8a513939620a11a409edec9cb6e
kingavatar <saikiranreddy06@gmail.com>
2021-03-25 21:25:01 +0530

    Scaled and Zoomed to Catalonia

--- src/components/ArcGis.vue
+++ src/components/ArcGis.vue
@@ -7,23 +7,37 @@ import Vue from "vue";
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 export default Vue.extend({
+  data(){
+    return{
+      map:new Map({
+      basemap: "hybrid",
+      ground: "world-elevation",
+    }),
+      view: new MapView()
+    }
+  },
   mounted() {
-    var map = new Map({
+    this.map = new Map({
       basemap: "hybrid",
       ground: "world-elevation",
     });
 
-    var view = new MapView({
+    this.view = new MapView({
       container: "viewDiv",
-      map: map,
+      map: this.map,
       // zoom: 10,
-      scale: 5000000,
-      center: [41.5912, 1.5209],
+      scale: 1749933.8984067584,
+      center:[1.7056684862856024,41.77040227096264],
       constraints: {
         // minZoom: 7, // Use this constraint to avoid zooming out too far
+        minScale:1749933.8984067584
       },
     });
+   this.view.popup.content = "Click a feature on the map to view its attributes";
   },
+  methods:{
+    
+  }
 });
 </script>
 
